91e83f3fd68c591ad7d17e77bc7b918a6a6416a5
Yuddomack <darr880406@gmail.com>
2020-03-04 19:02:19 +0900

    update: something

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,6 +20,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+app.get('/qq', (req, res) => {
+  res.render('test.html');
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
